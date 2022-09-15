8567051d99313676cc6c837d59721ccd632863f8
Cristhian Monrroy <monrroy88960@hotmail.com>
2022-09-15 16:39:56 -0500

    Creacion del proyecto con Jetstream

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array  $input
+     * @return \App\Models\User
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:80'],
+            'email' => ['required', 'string', 'email', 'max:120', 'unique:users'],
+            'mobile' => ['required', 'numeric', 'max_digits:40'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'mobile' => $input['mobile'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
